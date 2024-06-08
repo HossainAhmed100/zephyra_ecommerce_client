c060f5f879d2e2272071553f1ffe4e75b1a16662
HossainAhmed100 <successfulllife101@gmail.com>
2024-06-08 07:39:54 +0600

    Sidebar navlink updated

--- src/Layout/DashboardLayout.jsx
+++ src/Layout/DashboardLayout.jsx
@@ -88,13 +88,8 @@ function DashboardLayout() {
                 className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 group" end>
                 {({ isActive }) => (
                   <>
-                  <FaChartPie color={isActive ? "" : ""}/>
-                  <svg className={`flex-shrink-0 w-5 h-5 ${isActive ? 'text-gray-500' : 'text-gray-900'} transition duration-75   group-hover:text-gray-900`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z"/>
-                    <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z"/>
-                    <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z"/>
-                </svg>
-                <span className="flex-1 ms-3 whitespace-nowrap">Dashboard</span>
+                  <FaChartPie color={isActive ? "#000000" : "#6b7280"} size={22}/>
+                  <span className="flex-1 ms-3 whitespace-nowrap">Dashboard</span>
                   </>
                 )}
               </NavLink>
@@ -105,12 +100,8 @@ function DashboardLayout() {
                 className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 group" end>
                 {({ isActive }) => (
                   <>
-                  <svg className={`flex-shrink-0 w-5 h-5 ${isActive ? 'text-gray-500' : 'text-gray-900'} transition duration-75   group-hover:text-gray-900`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z"/>
-                    <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z"/>
-                    <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z"/>
-                </svg>
-                <span className="flex-1 ms-3 whitespace-nowrap">Manage Product</span>
+                  <FaChartPie color={isActive ? "#000000" : "#6b7280"} size={22}/>
+                  <span className="flex-1 ms-3 whitespace-nowrap">Manage Product</span>
                   </>
                 )}
               </NavLink>
@@ -121,12 +112,8 @@ function DashboardLayout() {
                 className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 group" end>
                 {({ isActive }) => (
                   <>
-                  <svg className={`flex-shrink-0 w-5 h-5 ${isActive ? 'text-gray-500' : 'text-gray-900'} transition duration-75   group-hover:text-gray-900`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z"/>
-                    <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z"/>
-                    <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z"/>
-                </svg>
-                <span className="flex-1 ms-3 whitespace-nowrap">Add New Product</span>
+                  <FaChartPie color={isActive ? "#000000" : "#6b7280"} size={22}/>
+                  <span className="flex-1 ms-3 whitespace-nowrap">Add New Product</span>
                   </>
                 )}
               </NavLink>
@@ -134,15 +121,15 @@ function DashboardLayout() {
               <li>
               <NavLink
                 to="myProfile"
-                className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 group" end>
+                className={({isActive}) => {
+                  return (
+                    `flex items-center p-2 text-gray-900 rounded-lg ${isActive && 'bg-gray-100'}  hover:bg-gray-100 group`
+                  )
+                }} end>
                 {({ isActive }) => (
                   <>
-                  <svg className={`flex-shrink-0 w-5 h-5 ${isActive ? 'text-gray-500' : 'text-gray-900'} transition duration-75   group-hover:text-gray-900`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z"/>
-                    <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z"/>
-                    <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z"/>
-                </svg>
-                <span className="flex-1 ms-3 whitespace-nowrap">My Profile</span>
+                  <FaChartPie color={isActive ? "#000000" : "#6b7280"} size={22}/>
+                  <span className="flex-1 ms-3 whitespace-nowrap">My Profile</span>
                   </>
                 )}
               </NavLink>
